44d1bd77bcb27661b34f4c8a80c8592913805cfc
AliWindsor <aliwindsor@gmail.com>
2021-07-16 19:11:57 +0100

    Initial Commit

---
+++ MusicBrainz Song Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicBrainz Song Search
+//
+//  Created by Alicia Windsor on 16/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
